d65e5d46c5b1360c3ebd57478aa95d48a997f2c4
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-06-17 10:46:32 -0300

    ok

--- usuariosEmail.js
+++ usuariosEmail.js
@@ -10,3 +10,20 @@ let usuarios = [
     email: "rui@gmail.com",
   },
 ];
+
+console.log("Qual email você quer procurar?");
+
+process.stdin.on("data", function (data) {
+  let email = data.toString().trim();
+  let usuario;
+  for (let i = 0; i < usuarios.length; i++) {
+    if (usuarios[i].email == email) {
+      usuario = usuarios[i];
+    }
+  }
+  if (usuario) {
+    console.log(usuario);
+  } else {
+    console.log("Usuario não encontrado");
+  }
+});
